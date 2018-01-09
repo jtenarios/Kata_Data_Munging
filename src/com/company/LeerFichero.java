98322a0bd584488c2bea002029ef5f5460407d89
Jaime Tena <jaime.tena@onyxcentersource.com>
2018-01-09 19:03:25 +0100

    Añado JoptionPane para mostrar el resultado final

--- src/com/company/LeerFichero.java
+++ src/com/company/LeerFichero.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -17,32 +18,27 @@ public class LeerFichero {
         Double max = null;
         Double min = null;
 
-        // Objeto de tipo Registro
-        Registro miRegistro = new Registro();
-
         // Declaración del ArrayList de tipo Registro
         ArrayList<Registro> wather = new ArrayList<Registro>();
-
 
         FileReader fr = new FileReader(archivo);
         BufferedReader br = new BufferedReader(fr);
 
-        cadena = br.readLine();
-
-        while (cadena != null){
+        while ((cadena = br.readLine()) != null){
 
             cadena = cadena.replace("*", " "); // Elimino los espacios
 
-
             // Codigo susceptible a fallar
             try{
+
                 dy  = cadena.substring(0, Coodenadas.DAYNUMBER);
                 max = Double.parseDouble(cadena.substring(Coodenadas.DAYNUMBER, Coodenadas.MAXTEMP));
                 min = Double.parseDouble(cadena.substring(Coodenadas.MAXTEMP, Coodenadas.MINTEMP));
 
-                //System.out.println(cadena);
                 System.out.println(dy + " - " + max + " - " + min + " - " + (max - min));
-                System.out.println("-------------------------");
+
+                // Objeto de tipo Registro
+                Registro miRegistro = new Registro();
 
                 // Le asigno los valores
                 miRegistro.setDay(dy);
@@ -53,42 +49,36 @@ public class LeerFichero {
                 // Anyado el nuevo registro
                 wather.add(miRegistro);
             }
-            catch(Exception StringIndexOutOfBoundsException){
+            catch(Exception StringIndexOutOfBoundsException) {
 
                 //dy  = "Registro ignorado";
                 //max = 0.0;
                 //min = 0.0;
-                StringIndexOutOfBoundsException.printStackTrace();
-                System.out.println("Registro ignorado");
-            } finally {
-                cadena = br.readLine();
+                //StringIndexOutOfBoundsException.printStackTrace();
+                System.out.println("Registro ignorado --> " + cadena);
             }
         }
         br.close();
-        System.out.println("Fin del bulce");
+        //System.out.println("Fin del bulce");
 
         // Mostrar el registro con la diferencia de temperatura menor
         Double minimaDiferencia = 99.9;
         int indiceRegistro = 0;
 
-        System.out.println("wather.size():" + wather.size());
+        //System.out.println("wather.size():" + wather.size());
         for (int i = 0; i < wather.size(); i++){
-
-            System.out.println(i);
-            System.out.println(wather.get(i).getDifTemp());
 
             if (wather.get(i).getDifTemp() < minimaDiferencia) {
                 indiceRegistro = i;
                 minimaDiferencia = wather.get(i).getDifTemp();
-                System.out.println("dia registrado");
             };
         }
 
-        System.out.println("El día con la menor diferencia de temperatura máxima y minima es el registro --> " + indiceRegistro);
-        System.out.println("Día: " + wather.get(indiceRegistro).getDay());
-        System.out.println("Máxima Temperatura: " + wather.get(indiceRegistro).getMaxTemp());
-        System.out.println("Minima Temperatura: " + wather.get(indiceRegistro).getMinTemp());
-        System.out.println("Diferencia: " + wather.get(indiceRegistro).getDifTemp());
+        JOptionPane.showMessageDialog(null, "El día con la menor diferencia de temperatura máxima y minima es:" +
+                                                                    "\nDía: " + wather.get(indiceRegistro).getDay() +
+                                                                    "\nMáxima Temperatura: " + wather.get(indiceRegistro).getMaxTemp() +
+                                                                    "\nMinima Temperatura: " + wather.get(indiceRegistro).getMinTemp() +
+                                                                    "\nDiferencia: " + wather.get(indiceRegistro).getDifTemp());
 
     }
 
